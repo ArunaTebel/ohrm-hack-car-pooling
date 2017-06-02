38bdd65e7944f6e152a3905fe7ec631ae948961f
ArunaTebel <arunatebel@hotmail.com>
2017-06-03 01:57:07 +0530

    Initial Commit

--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,47 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+    .factory('Drivers', function () {
+        var drivers = [{
+            id: 0,
+            name: 'Ben Sparrow',
+            lastText: 'BMW M3',
+            face: 'img/ben.png'
+        }, {
+            id: 1,
+            name: 'Max Lynx',
+            lastText: 'McLaren F1',
+            face: 'img/max.png'
+        }, {
+            id: 2,
+            name: 'Adam Bradleyson',
+            lastText: 'Ferrari F50',
+            face: 'img/adam.jpg'
+        }, {
+            id: 3,
+            name: 'Perry Governor',
+            lastText: 'Ford Mustang',
+            face: 'img/perry.png'
+        }, {
+            id: 4,
+            name: 'Mike Harrington',
+            lastText: 'Bugatti Veyron.',
+            face: 'img/mike.png'
+        }];
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
-  };
-});
+        return {
+            all: function () {
+                return drivers;
+            },
+            remove: function (chat) {
+                drivers.splice(drivers.indexOf(chat), 1);
+            },
+            get: function (chatId) {
+                for (var i = 0; i < drivers.length; i++) {
+                    if (drivers[i].id === parseInt(chatId)) {
+                        return drivers[i];
+                    }
+                }
+                return null;
+            }
+        };
+    });
