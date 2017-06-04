cdc2a282d39f2d0e0fbb54179db6015d84a4eb23
ArunaTebel <arunatebel@hotmail.com>
2017-06-04 12:06:02 +0530

    login implemented

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -26,4 +26,87 @@ angular.module('starter.controllers', [])
         $scope.settings = {
             enableFriends: true
         };
+
+        $scope.toggleChange = function () {
+            // if ($scope.value == false) {
+            //   $scope.value = true;
+            // } else
+            //   $scope.value = false;
+            console.log($scope.settings);
+        };
+    })
+
+    .controller('LoginCtrl', function ($scope, $stateParams, Drivers, $state, LoginService) {
+        $scope.title = "Login";
+        $scope.obj = {};
+
+        $scope.formSubmit = function () {
+            if (LoginService.login($scope.obj.username, $scope.obj.password)) {
+                $scope.error = '';
+                $scope.username = '';
+                $scope.password = '';
+                $state.transitionTo('tab.profile');
+            } else {
+                $scope.error = "Incorrect username/password !";
+            }
+        };
+    })
+
+    .factory('LoginService', function ($http, $httpParamSerializer) {
+        var admin = 'admin';
+        var pass = 'pass';
+        var isAuthenticated = false;
+
+        return {
+            login: function (username, password) {
+                var issueTokenRequest = {
+                    method: 'POST',
+                    url: '/oauth/issueToken',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: {
+                        grant_type: 'client_credentials',
+                        client_id: 'aruna',
+                        client_secret: 'aruna'
+                    }
+                };
+
+
+                $http(issueTokenRequest).then(
+                    function (response) {
+                        if (response.data.access_token) {
+                            var loginRequest = {
+                                method: 'POST',
+                                url: '/api/v1/login',
+                                headers: {
+                                    'Content-Type': 'application/x-www-form-urlencoded',
+                                    'Authorization': 'Bearer ' + response.data.access_token
+                                },
+                                data: "username=" + username + "&password=" + password
+                            };
+                            $http(loginRequest).then(function (response) {
+                                if (response.data.login) {
+                                    localStorage.loggedInUser = angular.toJson(response.data);
+                                }
+                            });
+
+                        } else {
+                            console.log('Access Token Not Recieved!');
+                        }
+                    },
+                    function () {
+
+                    }
+                );
+
+
+                isAuthenticated = username === admin && password === pass;
+                return isAuthenticated;
+            },
+            isAuthenticated: function () {
+                return isAuthenticated;
+            }
+        };
+
     });
